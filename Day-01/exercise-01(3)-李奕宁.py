6fea8660d5bd4ce5a0fd626bc6ab50da90d17c7e
Elaineyining <64621255+Elaineyining@users.noreply.github.com>
2020-05-03 10:10:13 +0800

    Create exercise-01(3)-李奕宁.py

---
+++ Day-01/exercise-01(3)-李奕宁.py
@@ -0,0 +1,7 @@
+name = input("tell me your name,and i will repeat it back to you:")
+print(name)
+age = input("tell me your age?")
+if int(age) >=18:
+    print("age >=18")
+else:
+    print("age<18")
