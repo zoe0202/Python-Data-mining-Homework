8c3d6a505a992b396b01a565533d6ea5458dcde3
NealPayne <63768254+NealPayne@users.noreply.github.com>
2020-05-05 01:36:45 +0800

    作业

--- Day-03/Exercise-02-杨俊坤（1）.py
+++
@@ -1,3 +0,0 @@
-Fahrenheit = float(input('Write your Fahrenheit here:'))
-Celsius = float((Fahrenheit-32)/1.8)
-print('Convert to', Celsius ,' ℃')
--- Day-03/Exercise-02-杨俊坤（3）.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-import math
-
-Radius = float(input('Write the Radius here:'))
-Circumference = round(Radius*math.pi,2)
-Area = round(Radius*Radius*math.pi,2)
-print('The Circumference is',Circumference)
-print('The Area is',Area)
