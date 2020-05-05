270da581c6884b9f2e4e2c31275d9b6ece0ed590
Lu-zheng <64631513+Lu-zheng@users.noreply.github.com>
2020-05-05 14:30:25 +0800

    刘雨萱第五天作业
    刘雨萱第五天作业

---
+++ Day-05/Exercise-04-刘雨萱/将列表转换为字符串，并使用逗号分隔列表中的各个元素.py
@@ -0,0 +1,5 @@
+list=[input("请创建一个列表：")]
+str1=str(list)
+str2=str1[2:-2]
+print(str2)
+print(len(str2))
---
+++ Day-05/Exercise-04-刘雨萱/根据用户输入的文件名，返回文件的后缀名.py
@@ -0,0 +1,3 @@
+filename=input("请输入文件名:")
+name=filename.split(".")
+print("文件后缀名为：",name[-1])
