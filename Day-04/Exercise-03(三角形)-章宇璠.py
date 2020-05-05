de663c338b54dcb13a63834a0c013aa38f618ae1
Evanna-Fox <wywby3@126.com>
2020-05-05 19:23:32 +0800

    Exercise-03-章宇璠
    Exercise-03-章宇璠

---
+++ Day-04/Exercise-03(三角形)-章宇璠.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    a = float(input('a = '))
+    b = float(input('b = '))
+    c = float(input('c = '))
+    if a + b > c and a + c > b and b + c > a:
+        print('周长: %f' % (a + b + c))
+        p = (a + b + c) / 2
+        area = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+        print('面积: %f' % (area))
+    else:
+        print('不能构成三角形')
