ab1364581e553e846c264822a7e0d88712b9e315
zhuwei233 <64632884+zhuwei233@users.noreply.github.com>
2020-05-06 09:47:51 +0800

    祝薇
    之前没有打包压缩，好像命名就没按格式来。现在整理了一下，辛苦师哥了

--- Day-04/Exercise-03-祝薇/判断质数-祝薇.py
+++
@@ -1,7 +0,0 @@
-n = int(input("num:"))
-for i in range(2, n):
-    if n%i==0:
-        print("N")
-        break
-else:
-    print("Y")
--- Day-05/Exercise-04-祝薇/逗号分隔-祝薇.py
+++
@@ -1,8 +0,0 @@
-L=[1,2,3,4,5]
-s1=','.join(str(n) for n in L)
-print(s1)
-l=[1,2,3,4,5,6,7]
-o=''
-for i in l:
-    o+=str(i)+','
-print(o[:-1])
