663d014fa7deab039abb2797cd7b52543070ac13
CBB1020 <cbb506@163.com>
2020-05-13 20:33:20 +0800

    这是一个作业
    day10作业

--- Day-10/exercise-08-程柏冰.py
+++ Day-10/exercise-08-程柏冰.py
@@ -7,16 +7,24 @@ import threading
 def test():
     for i in range(1, 11):
         time.sleep(0.5)
-        print(i)
+        print(threading.current_thread().name +':'+str(i))
 
 def main():
-    added_thread = threading.Thread(target=test)
+    thread1 = threading.Thread(target=test)
+    thread1.start()
     thread2 = threading.Thread(target=test)
+    thread2.start()
     thread3 = threading.Thread(target=test)
+    thread3.start()
     thread4 = threading.Thread(target=test)
+    thread4.start()
     thread5 = threading.Thread(target=test)
-    added_thread.start()
-    added_thread.join()
+    thread5.start()
+    thread1.join()
+    thread2.join()
+    thread3.join()
+    thread4.join()
+    thread5.join()
     print('all done')
 
 if __name__ == '__main__':
