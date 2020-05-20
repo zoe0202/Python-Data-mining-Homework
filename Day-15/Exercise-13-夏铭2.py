6362a7bd18643dfe9177ef8eb8cd3e8537d7b3f4
xmmm122 <64626653+xmmm122@users.noreply.github.com>
2020-05-21 01:28:52 +0800

    夏铭
    之前有几个作业投错文件了...

---
+++ Day-15/Exercise-13-夏铭2.py
@@ -0,0 +1,7 @@
+import requests
+
+url = 'https://movie.douban.com/subject/1292052/'
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"}
+data = requests.get(url,headers=headers).text
+
+print(data)
