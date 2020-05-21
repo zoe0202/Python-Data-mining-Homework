86620285e7fb2701e1b6216cfbce8e5c818cdac7
xiaozhaoer <62042428+xiaozhaoer@users.noreply.github.com>
2020-05-21 19:15:24 +0800

    徐昭第15天作业修改版

--- Day-15/Exercise-13-徐昭/Exercise-13-徐昭.2.py
+++ Day-15/Exercise-13-徐昭/Exercise-13-徐昭.2.py
@@ -6,7 +6,6 @@
 import requests
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 SE 2.X MetaSr 1.0"}
-url = 'https://movie.douban.com/top250?start=0&filter='
 
 proxies = {
     "http": "http://123.207.96.189:80"
